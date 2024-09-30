3899f2db9f2169ef5cdcabdef98cd77c883ee392
Bui Thai Hoang <154488027+BuiThaiHoang267@users.noreply.github.com>
2024-09-30 16:54:11 +0700

    Add references, add nuget packet EF Core

---
+++ FastFoodManagement.Model/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using FastFoodManagement.Model.Abstract;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FastFoodManagement.Model.Models
+{
+    [Table("Categories")]
+    public class Category : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = default!;
+
+    }
+}
