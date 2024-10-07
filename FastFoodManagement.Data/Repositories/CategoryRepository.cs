7ab637eb5e1c73a97b21f13ab0956f3849e074fe
Bui Thai Hoang <154488027+BuiThaiHoang267@users.noreply.github.com>
2024-10-07 16:03:22 +0700

    Product and Category Repository

---
+++ FastFoodManagement.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using FastFoodManagement.Data.Infrastructure;
+using FastFoodManagement.Model.Models;
+using System;
+using System.Linq;
+
+
+namespace FastFoodManagement.Data.Repositories
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        // Add more specific methods here when necessary
+    }
+    public class CategoryRepository : RepositoryBase<Category>, ICategoryRepository
+    {
+        public CategoryRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+    }
+}
